40ab9e6f40cb929ed2502bad6d1a8fa1a5bb589d
Nicolas Zambrano Madero <30539056+DevNicolas@users.noreply.github.com>
2019-10-30 08:22:28 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ def result_search():
 
 @app.route('/profile/<string:title>', methods=["GET"])
 def profile():
-    complement = request.args.get['title']
+    complement = request.args.get('title')
     url = request.get("https://scholar.google.com"+complement)
     bsObj = BeautifulSoup(url.content, "html.parser")
     perfil = bsObj.findAll("div", {"id": "gsc_prf_i"})
